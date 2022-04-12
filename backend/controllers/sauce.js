ba27b9e76b96d613698279e4791432743aa877b7
Alicia Petitpas <52076207+xayalicia@users.noreply.github.com>
2022-04-12 16:07:35 +0200

    Changement message erreur mdp et meilleure sécurité modification/suppression sauce

--- backend/controllers/sauce.js
+++ backend/controllers/sauce.js
@@ -18,33 +18,57 @@ exports.createSauce = (req, res, next) => {
 
 //Modification
 exports.modifySauce = (req, res, next) => {
-    Sauce.findOne({ _id: req.params.id })
-        .then(sauce => {
-            const filename = sauce.imageUrl.split('/images/')[1];
-            fs.unlink(`images/${filename}`, () => {
-                const sauceObject = req.file ? {
-                    ...JSON.parse(req.body.sauce),
-                    imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
-                } : {...req.body };
-                Sauce.updateOne({ _id: req.params.id }, {...sauceObject, _id: req.params.id })
-                    .then(() => res.status(200).json({ message: 'Object modified' }))
-                    .catch(error => res.status(400).json({ error }));
-            });
-        })
+    Sauce.findOne({ _id: req.params.id }).then(
+            (sauce) => {
+                //Si on ne trouve pas la sauce recherchée
+                if (!sauce) {
+                    res.status(404).json({
+                        error: new Error('No such object')
+                    });
+                }
+                //Si l'ID de l'user ne correspond pas à celui qui possède l'objet
+                if (thing.userId !== req.auth.userId) {
+                    res.status(400).json({
+                        error: new Error('Unauthorized request')
+                    });
+                }
+                const filename = Sauce.imageUrl.split('/images/')[1];
+                fs.unlink(`images/${filename}`, () => {
+                    const sauceObject = req.file ? {
+                        ...JSON.parse(req.body.sauce),
+                        imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
+                    } : {...req.body };
+                    Sauce.updateOne({ _id: req.params.id }, {...sauceObject, _id: req.params.id })
+                        .then(() => res.status(200).json({ message: 'Object modified' }))
+                        .catch(error => res.status(400).json({ error }));
+                });
+            })
         .catch(error => res.status(500).json({ error }));
 };
 
 //Suppression
 exports.deleteSauce = (req, res, next) => {
-    Sauce.findOne({ _id: req.params.id })
-        .then(sauce => {
-            const filename = sauce.imageUrl.split('/images/')[1];
-            fs.unlink(`images/${filename}`, () => {
-                Sauce.deleteOne({ _id: req.params.id })
-                    .then(() => res.status(200).json({ message: 'Object deleted' }))
-                    .catch(error => res.status(400).json({ error }));
-            });
-        })
+    Sauce.findOne({ _id: req.params.id }).then(
+            (sauce) => {
+                //Si on ne trouve pas la sauce recherchée
+                if (!sauce) {
+                    res.status(404).json({
+                        error: new Error('No such object')
+                    });
+                }
+                //Si l'ID de l'user ne correspond pas à celui qui possède l'objet
+                if (thing.userId !== req.auth.userId) {
+                    res.status(400).json({
+                        error: new Error('Unauthorized request')
+                    });
+                }
+                const filename = sauce.imageUrl.split('/images/')[1];
+                fs.unlink(`images/${filename}`, () => {
+                    Sauce.deleteOne({ _id: req.params.id })
+                        .then(() => res.status(200).json({ message: 'Object deleted' }))
+                        .catch(error => res.status(400).json({ error }));
+                });
+            })
         .catch(error => res.status(500).json({ error }));
 };
 
--- backend/middleware/checkPassword.js
+++ backend/middleware/checkPassword.js
@@ -4,7 +4,7 @@ const passwordSchema = require('../models/password');
 //on vérifie que le mot de passe respecte le schéma
 module.exports = (req, res, next) => {
     if (!passwordSchema.validate(req.body.password)) {
-        return res.status(400).json({ message: 'Password needs at least 8 characters, no space and minimum 2 digits.' });
+        return res.status(400).json({ message: 'Password needs at least 8 characters, no space and minimum 2 digits with at least one uppercase and one lowercase.' });
     } else {
         next();
     }
